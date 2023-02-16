eff5a11cb6498c496ee36d85c7344a04b47b471e
gros-pataplouf <sceres@pm.me>
2023-02-16 22:44:10 +0100

    add dynamic marker

--- planttracker_project/planttracker_frontend/src/components/Track.jsx
+++ planttracker_project/planttracker_frontend/src/components/Track.jsx
@@ -1,20 +1,44 @@
 import axios from 'axios';
 import { API_URL_LOCATIONS } from '../constants';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
-import { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
+import { useState } from 'react';
 import { useGeolocated } from "react-geolocated";
 
 
 
-export default function Track() {
 
-const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-        useGeolocated({
-            positionOptions: {
-                enableHighAccuracy: false,
-            },
-            userDecisionTimeout: 5000,
-        });
+function DynamicMarker({children, location}) {
+    const [position, setPosition] = useState(location);
+
+    let center;
+    const map = useMapEvents({
+      drag() {
+        center = map.getCenter();
+        setPosition(center);
+      },
+      click() {
+        center = map.getCenter();
+        setPosition(center);
+      }
+    })
+    return !position? 
+null
+     : (
+    <Marker position={position}>
+        {children}
+    </Marker>);
+  }
+  
+
+export default function Track() {
+    const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+            useGeolocated({
+                positionOptions: {
+                    enableHighAccuracy: true,
+                },
+                userDecisionTimeout: 5000,
+            });
+
 
     return (!isGeolocationAvailable ? (
                 <div>Your browser does not support Geolocation</div>
@@ -22,20 +46,25 @@ const { coords, isGeolocationAvailable, isGeolocationEnabled } =
                 <div>Geolocation is not enabled</div>
             ) : coords ? (
 
-                <MapContainer id="map" center={[9.86409912109375, 54.06325355147857]} zoom={8} scrollWheelZoom={false}>
+                <MapContainer 
+                id="map"
+                center={[coords.latitude, coords.longitude]}
+                zoom={13}
+                scrollWheelZoom={false}
+                
+                >
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
                 />
-                                <Marker position={[coords.longitude, coords.longitude]}>
-                <Popup> "Hello" </Popup>
-            </Marker>
 
-
+                <DynamicMarker location={[coords.latitude, coords.longitude]}>
+                    <Popup>blah</Popup>
+                </DynamicMarker>
               </MapContainer>
 
             ) : (
-                <div>Getting the location data&hellip; </div>
+                <div>Getting the location data &hellip; </div>
             )
         
 
