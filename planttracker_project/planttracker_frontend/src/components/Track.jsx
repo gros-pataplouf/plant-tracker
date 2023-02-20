064b18663e99093835d214d89794251296a780a4
gros-pataplouf <sceres@pm.me>
2023-02-20 22:31:57 +0100

    custom hook

--- planttracker_project/planttracker_frontend/src/components/Track.jsx
+++ planttracker_project/planttracker_frontend/src/components/Track.jsx
@@ -1,71 +1,35 @@
 import axios from 'axios';
 import { API_URL_LOCATIONS } from '../constants';
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useGeolocated } from "react-geolocated";
 import {debounce} from "../helpers/utils"
 
 import { API_OSM_NOMINATIM } from '../constants';
 
-export function useLocation(initialValue) {
-  const [value, setValue] = useState(initialValue);
-
-  function handleChange(e) {
-    setValue(e.target.value);
-  }
-
-  const inputProps = {
-    value: value,
-    onChange: handleChange
-  };
-
-  return inputProps;
-}
-
-
-
-function DefaultMap() {
-  return (  
-    <>
-  <SearchField/>
-  <MapContainer 
-  id="map"
-  center={[48, 8]}
-  zoom={5}
-  scrollWheelZoom={true}
-  
-  >
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
-  />
-
-  <DynamicMarker location={[48, 8]}>
-      <Popup>
-      <form class="formdata" method="post">
-      <label for="name">Name</label>
-        <input name="name" id="name"/>
-        <label for="location">Location</label>
-        <input name="location" id="location"/>
-        <div id="camcontainer">
-        <button id="cam">Cam</button>
-        <video class='hidden'></video>
-        <button class="take_photo"><img height="20px" width="20px" src="./public/icons/camera_material.svg" alt=""/></button>
-        </div>
-        <div id="imageSrc"><img src="" class="blob" alt=""/></div>
-        <button>Abschicken</button>
-
-      </form>
-      </Popup>
-  </DynamicMarker>
-</MapContainer>
-</>)
+function useLocation(initialValue) {
+const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+    useGeolocated({
+        positionOptions: {
+            enableHighAccuracy: false,
+        },
+        userDecisionTimeout: 5000,
+    });
+    if (coords) {
+      console.log(coords);
+      initialValue = [coords.latitude, coords.longitude];
+      
+    }
+    return initialValue;
 
 }
+
 
 function SearchField(props) {
   const {position} = props;
   const [results, setResults] = useState([]);
+
+
   function inputHandler() {
       const input = document.querySelector("#inputfield")
       const url = `${API_OSM_NOMINATIM}?q=${input.value}&limit=5&format=json`;
@@ -81,11 +45,13 @@ function SearchField(props) {
         const [filtered] = results.filter(i => i.place_id == dataindex);
         console.log([filtered.lon, filtered.lat])
       }
-    
+
+
   
 
   return (
       <>
+      <h2></h2>
       <input id="inputfield" onInput={debounce(inputHandler)}/>
       {results.map(
               _ => {return <div dataindex={_.place_id} key={_.place_id} onClick={clickHandler}>{_.display_name}</div>}
@@ -121,28 +87,16 @@ null
   }
 
 export default function Track() {
-  const [position, setPosition] = useState([])
 
-  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-  useGeolocated({
-      positionOptions: {
-          enableHighAccuracy: false,
-      },
-      userDecisionTimeout: 5000,
-  });
+  const coords = useLocation([50, 10]);
+  console.log(coords);
 
 
-
-    return (  !isGeolocationAvailable ? (
-                        <DefaultMap/>
-                    ) : !isGeolocationEnabled ? (
-                        <DefaultMap/>
-                    ) : coords ? (
-                <>
-                <SearchField position={[coords.longitude, coords.latitude]}/>
+    return (                <>
+                <SearchField position={coords}/>
                 <MapContainer 
                 id="map"
-                center={[coords.latitude, coords.longitude]}
+                center={coords}
                 zoom={5}
                 scrollWheelZoom={true}
                 
@@ -152,7 +106,7 @@ export default function Track() {
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
                 />
 
-                <DynamicMarker location={[coords.latitude, coords.longitude ]}>
+                <DynamicMarker location={coords}>
                     <Popup>
                     <form class="formdata" method="post">
                     <label for="name">Name</label>
@@ -171,8 +125,5 @@ export default function Track() {
                     </Popup>
                 </DynamicMarker>
               </MapContainer>
-              </>) : (
-                <div>Getting location data</div>
-              )
-
+              </>
             )}
