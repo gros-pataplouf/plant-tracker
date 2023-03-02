2f4f480c66c5cd3af806f374dcb2fd04ed696034
gros-pataplouf <sceres@pm.me>
2023-03-02 22:41:40 +0100

    add form submission

--- planttracker_project/planttracker_frontend/src/components/Track.jsx
+++ planttracker_project/planttracker_frontend/src/components/Track.jsx
@@ -1,22 +1,51 @@
 import axios from 'axios';
-import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap, useMapEvents } from 'react-leaflet';
 import { useEffect, useState } from 'react';
 import { useGeolocated } from "react-geolocated";
 import { debounce } from "../helpers/utils"
-import { API_OSM_NOMINATIM } from '../constants';
+import { API_OSM_NOMINATIM, API_URL_LOCATIONS } from '../constants';
+
+
+// location model
+// class Location(models.Model):
+//     location = models.PointField()
+//     area = models.IntegerField()
+//     image = models.ImageField(blank=True) #upload_to='users/%Y/%m/%d/', 
+//     description = models.CharField(max_length=100)
+//     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+//     plant = models.ForeignKey(Plant, on_delete=models.PROTECT)
+
+
+function Form({location}) {
+  return (
+    <form method="post">
+      <input type="hidden" name="location" value={location} />
+      <input type="number" name="area" />
+      <input type="text" name="description" />
+      <select name="plant" id="plant-selection">
+          <option value="">Select a species</option>
+          <option ></option>
+          <option ></option>
+          <option ></option>
+          <option ></option>
+          <option ></option>
+          <option ></option>
+      </select>
+      <input type="submit" value="Submit" />
+    </form>
+  )
+}
 
 function CenterAutomatically({location, setLocation}) {
   const map = useMap();
-
    useEffect(() => {
-     map.setView(location);
-     map.panTo(location);
-     map.setZoom(10);
+    map.flyTo(location, map.getZoom())
    }, [location]);
    return null;
  }
  
-function LocatedMap({location, setLocation, coords}) {
+function Map({props}) {
+  const  {location, setLocation, coords} = props;
   useEffect(() => {
     if (coords) {
       setLocation([coords.latitude, coords.longitude])
@@ -28,32 +57,12 @@ function LocatedMap({location, setLocation, coords}) {
   center={location}
   zoom={5}
   scrollWheelZoom={true}
-  >
-
-
-  <TileLayer
+  >  <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
   />
 
-  <DynamicMarker location={location}>
-      <Popup>
-      <form class="formdata" method="post">
-      <label for="name">Name</label>
-        <input name="name" id="name"/>
-        <label for="location">Location</label>
-        <input name="location" id="location"/>
-        <div id="camcontainer">
-        <button id="cam">Cam</button>
-        <video class='hidden'></video>
-        <button class="take_photo"><img height="20px" width="20px" src="./public/icons/camera_material.svg" alt=""/></button>
-        </div>
-        <div id="imageSrc"><img src="" class="blob" alt=""/></div>
-        <button>Abschicken</button>
-
-      </form>
-      </Popup>
-  </DynamicMarker>
+  <DynamicMarker location={location} setLocation={setLocation}/>
   <CenterAutomatically location={location} setLocation={setLocation} props={{location, setLocation}}/>
 
 </MapContainer>
@@ -62,13 +71,12 @@ function LocatedMap({location, setLocation, coords}) {
 
 
 function SearchField({props}) {
-  const {setLocation, location, coords}
-  = props;
+  const {setLocation, location, coords} = props;
   const [results, setResults] = useState([]);
+  const inputField = document.querySelector("input");
 
   function inputHandler() {
-      const input = document.querySelector("#inputfield")
-      const url = `${API_OSM_NOMINATIM}?q=${input.value}&limit=5&format=json`;
+    const url = `${API_OSM_NOMINATIM}?q=${inputField.value}&limit=5&format=json`;
       axios.get(url)
       .then(res => {
           setResults(res.data);
@@ -79,52 +87,64 @@ function SearchField({props}) {
       function clickHandler(e) {
         const dataindex = parseInt(e.target.getAttribute("dataindex"));
         const [filtered] = results.filter(i => i.place_id == dataindex);
-        setLocation([parseFloat(filtered.lat), parseFloat(filtered.lon)])
+        setLocation([parseFloat(filtered.lat), parseFloat(filtered.lon)]);
+        setResults([]);
+        inputField.value = "";
       }
+      
       function clickHandlerButton(e) {
-          setLocation([coords.latitude, coords.longitude])
+        inputField.value = "";
+        setResults([]);
+        setLocation([coords.latitude, coords.longitude]);
       }
 
   return (
       <>
-      <input id="inputfield" onInput={debounce(inputHandler)}/>
+      <input id="inputfield" onChange={debounce(inputHandler)}/>
       {coords && <button onClick={clickHandlerButton} coords={coords}>Go Back to detected location</button>}
       {results.map(
               _ => {return <div dataindex={_.place_id} key={_.place_id} onClick={clickHandler}>{_.display_name}</div>}
           )}
- 
       </>
+  )
+}
 
+function ContinueButton({props}) {
+  const {setDisplay} = props;
+  function clickHandler() {
+    setDisplay("form");
+  }
+  return (
+    <button onClick={clickHandler}>Continue</button>
+  )
+}
+
+function BackButton({props}) {
+  const {setDisplay} = props;
+  function clickHandler() {
+    setDisplay("map");
+  }
+  return (
+    <button onClick={clickHandler}>Back</button>
   )
 }
 
 
-function DynamicMarker({children, location}) {
-    const [position, setPosition] = useState(location);
-
+function DynamicMarker(props) {
+    const {location, setLocation} = props;
     let center;
-    useEffect(() => {setPosition(location)}, [location])
     const map = useMapEvents({
       drag() {
         center = map.getCenter();
-        setPosition(center);
-      },
-      click() {
-        center = map.getCenter();
-        setPosition(center);
-
+        setLocation(center);
       }
     })
-    return !position? 
-null
-     : (
-    <Marker position={position}>
-        {children}
-    </Marker>);
+    return (<Marker position={location}/>)
   }
 
 export default function Track() {
-  const [location, setLocation] = useState([30, 0]);
+  const [location, setLocation] = useState([50, 10]);
+  const [display, setDisplay] = useState('map');
   const {
     coords,
     getPosition,
@@ -137,17 +157,23 @@ export default function Track() {
         enableHighAccuracy: false,
     },
     userDecisionTimeout: 5000,
+    watchPosition: true
 });
 
-
+if (display === "map"){
 
 return (!isGeolocationAvailable || !isGeolocationEnabled || coords) ? (
   <>
   <SearchField props={{setLocation, location, coords}}/>
-  <LocatedMap setLocation={setLocation} location={location} coords={coords}/>
+  <Map props={{location, setLocation, coords}}/>
+  <ContinueButton props={{location, setDisplay}}/>
 </>
 ) 
 : (
   <div>Getting the location data&hellip; </div>
 );
-}
+} else if (display === "form") {
+  return (
+    <Form props={{location}}/>
+  )
+}} 
