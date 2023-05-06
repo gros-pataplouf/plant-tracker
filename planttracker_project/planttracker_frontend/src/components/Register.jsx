23c8954f162b5187e25a7e9d770c193a7e602da6
gros-pataplouf <sceres@pm.me>
2023-05-06 13:20:06 +0200

    fix: style Register.jsx

--- planttracker_project/planttracker_frontend/src/components/Register.jsx
+++ planttracker_project/planttracker_frontend/src/components/Register.jsx
@@ -1,15 +1,24 @@
 import axiosInstance from '../helpers/axios';
 import { useState } from 'react';
 import { testMail, testPassword } from '../helpers/checks';
+import info from '../assets/icons/info.svg';
 
 const classes = {
-  wrapper: '',
+  wrapper: 'flex flex-col justify-between m-auto p-8',
   title: '',
-  form: '', 
+  form: 'flex flex-col ', 
   input: '',
-  label: '',
-  error: ''
+  label: 'mt-4 mb-2',
+  errorInput: 'border-red-800 active:outline-red-800',
+  errorSpan:  "text-red-800 italic",
+  tooltipIcon: "inline w-7 align-top",
+  tooltipSpan: "relative",
+  tooltipDiv: "absolute w-[80vw] bg-black top-4 p-4 border-spacing-2 border-2 rounded-3xl text-yellow-50 hidden m-4 leading-none",
+  btn: 'btn mt-8',
+  success: 'font-bold my-[50%]',
+  failure: 'font-bold'
 }
+
 
 export default function Register() {
     const [ emailErr, setEmailErr ] =  useState('');
@@ -36,10 +45,10 @@ export default function Register() {
       if (!email || !username || !password || !passwordConfirmation) {
         setIncompleteErr("Please fill out all required fields.")
         } 
-      
+     
       if ( email && !testMail(email)) {
         setEmailErr('Invalid email.')
- 
+
       } 
 
       if (password && password !==  passwordConfirmation) {
@@ -69,36 +78,38 @@ export default function Register() {
           .then(res => {setMessage(res.data);
           setSuccess(true)})
           .catch(error => setMessage(error.response.data))}
-
     return (
-      <div className="prose flex flex-col justify-center m-4">
-      <h2 className='my-8'>Sign up and contribute 💚</h2>
+      <div className={classes.wrapper}>
+      <h3 className={classes.title}>Sign up and contribute 💚</h3>
       {!success && 
-      <form className='flex flex-col' id='registrationForm' onSubmit={submitHandler} onChange={validateForm}>
-        <label htmlFor='email'>Email<span className="text-red-800 italic"> {emailErr}</span></label>
-        <input name='email' type='email' id='email' autoComplete='email' className={emailErr? "border-red-800 active:outline-red-800": 'undefined'} placeholder='required'/>
-        <label className='mt-4' htmlFor='username'>Username</label>
+      <form className={classes.form} id='registrationForm' onSubmit={submitHandler} onChange={validateForm}>
+        <label className={classes.label} htmlFor='email'>Email<span className={classes.errorSpan}> {emailErr}</span></label>
+        <input name='email' type='email' id='email' autoComplete='email' className={emailErr? classes.errorInput: 'undefined'} placeholder='required'/>
+        <label className={classes.label} htmlFor='username'>Username</label>
         <input name='username' type='text' id='username' autoComplete='username' placeholder='required'/>
-        <label htmlFor='password' className='mt-4'>Password {pwdErr && <span className="text-red-800 italic mr-0">{pwdErr}  
-        <span>  
-          <svg className='fill-red-800 inline absolute'
-            onClick={()=>{
+        <label className={classes.label} htmlFor='password'>Password {pwdErr && <span className={classes.errorSpan}>{pwdErr}  
+        <span className={classes.tooltipSpan} onClick={()=>{
               const tooltip = document.querySelector("#tooltip");
-              tooltip.classList.toggle("hidden")}}
-          xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24">
-            <path d="M453 776h60V536h-60v240Zm26.982-314q14.018 0 23.518-9.2T513 430q0-14.45-9.482-24.225-9.483-9.775-23.5-9.775-14.018 0-23.518 9.775T447 430q0 13.6 9.482 22.8 9.483 9.2 23.5 9.2Zm.284 514q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80 658.319 80 575.5q0-82.819 31.5-155.659Q143 347 197.5 293t127.341-85.5Q397.681 176 480.5 176q82.819 0 155.659 31.5Q709 239 763 293t85.5 127Q880 493 880 575.734q0 82.734-31.5 155.5T763 858.316q-54 54.316-127 86Q563 976 480.266 976Zm.234-60Q622 916 721 816.5t99-241Q820 434 721.188 335 622.375 236 480 236q-141 0-240.5 98.812Q140 433.625 140 576q0 141 99.5 240.5t241 99.5Zm-.5-340Z"/></svg></span></span>}
+              tooltip.classList.toggle("hidden")}}><img className={classes.tooltipIcon} src={info} />
+        <div className={classes.tooltipDiv} id="tooltip">
+          Passwords must be at least 8 characters long with 1 upper case, 1 lower case letter, 1 number and 1 special character.
+        </div>
+        </span>
+        </span>
+        }
         </label>
         <input name='password' id='password' type='password' autoComplete='password' placeholder='required' className={pwdErr? "border-red-800": undefined}/>
-         {pwdErr && <p className="text-red-800 italic hidden m-0 p-0 leading-none" id="tooltip">
-          Passwords must be at least 8 characters long with 1 upper case, 1 lower case letter, 1 number and 1 special character.
-        </p>}
-        <label className='mt-4' htmlFor='passwordConfirmation'>Confirm password <span className="text-red-800 italic">{pwdConfErr}</span></label>
+        <label className={classes.label} htmlFor='passwordConfirmation'>Confirm password <span className={classes.errorSpan}>{pwdConfErr}</span></label>
         <input name='passwordConfirmation' id='passwordConfirmation' type='password' placeholder='required'/>
-        <button className="btn" type="submit">Create account</button>
-        <p className="text-red-800 italic">{incompleteErr}</p>
+        <button className={classes.btn} type="submit">Create account</button>
+        <p className={classes.errorSpan}>{incompleteErr}</p>
      </form>}
-     <p>Message{message}</p>
+
+     {success? <p className={success ? classes.success: classes.errorSpan}>✅ {message}</p> : message && <p className={classes.failure}>⚠️ {message}</p>}
+     
 </div>
 
     )
 }
+
+
