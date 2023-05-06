5990da2c4a4cfe208ea1adca5de8e556962d79df
gros-pataplouf <sceres@pm.me>
2023-05-06 10:23:43 +0200

    fix: finish PlantList.jsx

--- planttracker_project/planttracker_frontend/src/components/PlantList.jsx
+++ planttracker_project/planttracker_frontend/src/components/PlantList.jsx
@@ -4,11 +4,8 @@ import axiosInstance from '../helpers/axios';
 import { resizeTiles } from '../helpers/utils';
 import React from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
-import { IconContext } from "react-icons";
-
 import chevron_right from '../assets/icons/chevron_right.svg';
 import chevron_left from '../assets/icons/chevron_left.svg';
-import { AiOutlinePlusCircle } from 'react-icons/ai'
 
 
 const classes = {
@@ -25,8 +22,7 @@ const classes = {
   image: "w-full rounded-xl",
   link: 'font-bold text-emerald-800 italic active:text-emerald-950 active:underline',
   description: 'overflow-hidden',
-  readMore: 'absolute right-4 bottom-0 z-10 stroke-emerald-800 bg-white '
-
+  readMore: 'absolute right-0 font-bold bottom-0 rounded-full p-2 z-10 text-emerald-800 bg-white'
 }
 
 
@@ -46,10 +42,8 @@ export default function PlantList () {
 
 
     useEffect(() => {
-
       const plantDataPromise = axiosInstance.get('plants/');
       const plantImagesPromise = axiosInstance.get('plants/images/')
-
       Promise.all([plantDataPromise, plantImagesPromise]).then((values) => {
         setPlantList(values[0].data);
         setPlantImages(values[1].data);
@@ -57,8 +51,7 @@ export default function PlantList () {
       })
       .catch(err => console.error(err))
  
-    }, [])
-;
+    }, []);
 
     
     return (
@@ -70,11 +63,8 @@ export default function PlantList () {
 
             { !plantList ? (
                 <h4>No plant info available</h4> ) : (
-                
-                  
                   plantList.map(plant => (
                     <div key={plant.id} id='emblaSlide' className={classes.emblaSlide}>
-
                             <h3>{plant.common_name_en}</h3>
                             <p className={classes.scientific}>{plant.scientific_name}</p>
                             <div className={classes.imageContainer}>
@@ -86,14 +76,11 @@ export default function PlantList () {
                               </button>
                               <img className={classes.image} src={plantImages.filter(img => img.plant === plant.id)[0].image} alt=""/>
                             </div>
-                            
-                            
-                            <div className={classes.readMore}>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M453 776h60V610h167v-60H513V376h-60v174H280v60h173v166Zm27.266 200q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80 658.319 80 575.5q0-82.819 31.5-155.659Q143 347 197.5 293t127.341-85.5Q397.681 176 480.5 176q82.819 0 155.659 31.5Q709 239 763 293t85.5 127Q880 493 880 575.734q0 82.734-31.5 155.5T763 858.316q-54 54.316-127 86Q563 976 480.266 976Zm.234-60Q622 916 721 816.5t99-241Q820 434 721.188 335 622.375 236 480 236q-141 0-240.5 98.812Q140 433.625 140 576q0 141 99.5 240.5t241 99.5Zm-.5-340Z" fill="#065f46"/></svg>
-                            </div>
+                            <Link to={`${plant.id}/`} className={classes.readMore}>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M453 776h60V610h167v-60H513V376h-60v174H280v60h173v166Zm27.266 200q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80 658.319 80 575.5q0-82.819 31.5-155.659Q143 347 197.5 293t127.341-85.5Q397.681 176 480.5 176q82.819 0 155.659 31.5Q709 239 763 293t85.5 127Q880 493 880 575.734q0 82.734-31.5 155.5T763 858.316q-54 54.316-127 86Q563 976 480.266 976Zm.234-60Q622 916 721 816.5t99-241Q820 434 721.188 335 622.375 236 480 236q-141 0-240.5 98.812Q140 433.625 140 576q0 141 99.5 240.5t241 99.5Zm-.5-340Z" stroke="#065f46" strokeWidth='50' /></svg>
+                            </Link>
                             <p className={classes.description} id='description'>{plant.description_en}<Link className={classes.link} to={"/plants/" + plant.id}>Read more</Link></p>
                         </div>
-      
                 ) 
             )
           )}
@@ -103,4 +90,3 @@ export default function PlantList () {
       </div>
     );
   }
-
