145f724bbb1d44ab156f5e2f965b5e03dcf7d58a
gros-pataplouf <sceres@pm.me>
2023-05-08 14:55:42 +0200

    refactor: abstract EmblaCarousel

--- planttracker_project/planttracker_frontend/src/components/Carousel.jsx
+++ planttracker_project/planttracker_frontend/src/components/Carousel.jsx
@@ -1,62 +1,47 @@
-import { useState, useEffect, useCallback } from 'react';
-import { Link } from 'react-router-dom';
-import axiosInstance from '../helpers/axios';
-import { resizeTiles } from '../helpers/utils';
-import React from 'react';
+import { useCallback } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import chevron_right from '../assets/icons/chevron_right.svg';
 import chevron_left from '../assets/icons/chevron_left.svg';
 
-
 const classes = {
-  wrapper: '',
-  embla: '',
+  embla: 'relative',
   emblaContainer: 'flex ',
-  emblaSlide: 'emblaSlide relative border-b-8 border-white group/item h-auto flex-[0_0_95%] min-w-0 pb-4 space-y-2 p-2 bg-yellow-50 overflow-hidden rounded-xl shadow-lg shadow-slate-500/50 border-solid border-2 border-slate-300 m-4',
-  title: 'pt-6 ml-2 text-emerald-800',
-  name: '',
-  scientific: 'italic',
-  imageContainer: 'relative',
-  chevronLeft: 'group-first/item:hidden bg-slate-500/80 absolute rounded-[50%] top-[calc(50%-24px)] -left-6',
-  chevronRight: 'group-last/item:hidden bg-slate-500/80 absolute rounded-[50%] top-[calc(50%-24px)] -right-6',
-  image: "w-full rounded-xl",
-  link: 'font-bold text-emerald-800 italic active:text-emerald-950 active:underline',
-  description: 'overflow-hidden',
-  readMore: 'absolute right-0 font-bold bottom-0 rounded-full p-2 z-10 text-emerald-800 bg-white'
+  chevronLeft: 'bg-slate-500/80 absolute rounded-[50%] top-[50%] -left-6 z-10',
+  chevronRight: 'bg-slate-500/80 absolute rounded-[50%] top-[50%] -right-6 z-10',
 }
 
+export default function Carousel({children}) {
+  const [emblaRef, emblaApi] = useEmblaCarousel({loop: true});
 
-export default function Carousel (props) {
-    const [emblaRef, emblaApi] = useEmblaCarousel();
-    const [plantList, setPlantList] = useState(false);
-    const [plantImages, setPlantImages] = useState([]);
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
 
-    const scrollPrev = useCallback(() => {
-      if (emblaApi) emblaApi.scrollPrev()
-    }, [emblaApi])
-  
-    const scrollNext = useCallback(() => {
-      if (emblaApi) emblaApi.scrollNext()
-    }, [emblaApi])
-  
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
 
 
-    
-    return (
 
-                  plantList.map(plant => (
-                    <div key={plant.id} id='emblaSlide' className={classes.emblaSlide}>
-                            <div className={classes.imageContainer}>
-                              <button className={classes.chevronLeft} onClick={scrollPrev}>
-                                <img src={chevron_left} alt="" />
-                              </button>
-                              <button className={classes.chevronRight} onClick={scrollNext}>
-                                <img src={chevron_right} alt="" />
-                              </button>
-                              <img className={classes.image} src={plantImages.filter(img => img.plant === plant.id)[0].image} alt=""/>
-                            </div>
-                        </div>
-                ) 
-            )
-          )}
-  
+  return (
+    <div className={classes.embla}>
+    <button className={classes.chevronLeft} onClick={scrollPrev}>
+      <img src={chevron_left} alt="forward" />
+    </button>
+    <button className={classes.chevronRight} onClick={scrollNext}>
+      <img src={chevron_right} alt="back" />
+    </button>
+    <button className={classes.chevronLeft} onClick={scrollPrev}>
+      <img src={chevron_left} alt="" />
+    </button>
+    <button className={classes.chevronRight} onClick={scrollNext}>
+      <img src={chevron_right} alt="" />
+    </button>
+    <div  ref={emblaRef}>
+    <div className={classes.emblaContainer}>
+      {children}
+    </div>
+    </div>
+    </div>
+  )
+}
--- planttracker_project/planttracker_frontend/src/components/PlantList.jsx
+++ planttracker_project/planttracker_frontend/src/components/PlantList.jsx
@@ -1,24 +1,16 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axiosInstance from '../helpers/axios';
 import { resizeTiles } from '../helpers/utils';
 import React from 'react';
-import useEmblaCarousel from 'embla-carousel-react';
-import chevron_right from '../assets/icons/chevron_right.svg';
-import chevron_left from '../assets/icons/chevron_left.svg';
-
+import Carousel from './Carousel';
 
 const classes = {
   wrapper: '',
-  embla: '',
-  emblaContainer: 'flex ',
-  emblaSlide: 'emblaSlide relative border-b-8 border-white group/item h-auto flex-[0_0_95%] min-w-0 pb-4 space-y-2 p-2 bg-yellow-50 overflow-hidden rounded-xl shadow-lg shadow-slate-500/50 border-solid border-2 border-slate-300 m-4',
+  emblaSlide: 'emblaSlide relative border-b-8 border-white h-auto flex-[0_0_95%] min-w-0 pb-4 space-y-2 p-2 bg-yellow-50 overflow-hidden rounded-xl shadow-lg shadow-slate-500/50 border-solid border-2 border-slate-300 m-4',
   title: 'pt-6 ml-2 text-emerald-800',
   name: '',
   scientific: 'italic',
-  imageContainer: 'relative',
-  chevronLeft: 'group-first/item:hidden bg-slate-500/80 absolute rounded-[50%] top-[calc(50%-24px)] -left-6',
-  chevronRight: 'group-last/item:hidden bg-slate-500/80 absolute rounded-[50%] top-[calc(50%-24px)] -right-6',
   image: "w-full rounded-xl",
   link: 'font-bold text-emerald-800 italic active:text-emerald-950 active:underline',
   description: 'overflow-hidden',
@@ -26,19 +18,13 @@ const classes = {
 }
 
 
+
+
+
 export default function PlantList () {
-    const [emblaRef, emblaApi] = useEmblaCarousel();
     const [plantList, setPlantList] = useState(false);
     const [plantImages, setPlantImages] = useState([]);
 
-    const scrollPrev = useCallback(() => {
-      if (emblaApi) emblaApi.scrollPrev()
-    }, [emblaApi])
-  
-    const scrollNext = useCallback(() => {
-      if (emblaApi) emblaApi.scrollNext()
-    }, [emblaApi])
-  
 
 
     useEffect(() => {
@@ -57,25 +43,14 @@ export default function PlantList () {
     return (
       <div className={classes.wrapper} id='wrapper'>
         <h2 className={classes.title} id='title'>Invasive plants</h2>
-        <div className={classes.embla}>
-        <div  ref={emblaRef}>
-        <div className={classes.emblaContainer}>
-
+        <Carousel>
             { !plantList ? (
                 <h4>No plant info available</h4> ) : (
                   plantList.map(plant => (
                     <div key={plant.id} id='emblaSlide' className={classes.emblaSlide}>
                             <h3>{plant.common_name_en}</h3>
                             <p className={classes.scientific}>{plant.scientific_name}</p>
-                            <div className={classes.imageContainer}>
-                              <button className={classes.chevronLeft} onClick={scrollPrev}>
-                                <img src={chevron_left} alt="" />
-                              </button>
-                              <button className={classes.chevronRight} onClick={scrollNext}>
-                                <img src={chevron_right} alt="" />
-                              </button>
-                              <img className={classes.image} src={plantImages.filter(img => img.plant === plant.id)[0].image} alt=""/>
-                            </div>
+                            <img className={classes.image} src={plantImages.filter(img => img.plant === plant.id)[0].image} alt=""/>
                             <Link to={`${plant.id}/`} className={classes.readMore}>
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M453 776h60V610h167v-60H513V376h-60v174H280v60h173v166Zm27.266 200q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80 658.319 80 575.5q0-82.819 31.5-155.659Q143 347 197.5 293t127.341-85.5Q397.681 176 480.5 176q82.819 0 155.659 31.5Q709 239 763 293t85.5 127Q880 493 880 575.734q0 82.734-31.5 155.5T763 858.316q-54 54.316-127 86Q563 976 480.266 976Zm.234-60Q622 916 721 816.5t99-241Q820 434 721.188 335 622.375 236 480 236q-141 0-240.5 98.812Q140 433.625 140 576q0 141 99.5 240.5t241 99.5Zm-.5-340Z" stroke="#065f46" strokeWidth='50' /></svg>
                             </Link>
@@ -84,9 +59,8 @@ export default function PlantList () {
                 ) 
             )
           )}
+          </Carousel>
         </div>
-      </div>
-      </div>
-      </div>
+
     );
   }
