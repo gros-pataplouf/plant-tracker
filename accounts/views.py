e4d104d2079792d4b43bf643b54bc410378111d7
gros-pataplouf <sceres@pm.me>
2023-06-22 17:00:39 +0200

    fix: rewrite user emails

--- accounts/views.py
+++ accounts/views.py
@@ -83,17 +83,15 @@ class UserList(generics.CreateAPIView):
                 send_mail(
                 'Welcome to the Planttracker Project',
                 f"\
-                Dear Nature Lover🌻!
-                
-                Thank you for participating in the planttracker project.
+                Dear Nature Lover🌻!\
+                \
+                Thank you for participating in the planttracker project.\
                 Please click on the following link to activate your account:\
-                https://planttracker.onrender.com/activate?{uuid}.
-                
-                We are looking forward to your contributions as a citizen scientist.
-                
-                Your sincerely, 
+                https://planttracker.onrender.com/activate?{uuid}.\
+                We are looking forward to reviewing your contributions as a citizen scientist.\
+                \
+                Your sincerely,\
                 Your Planttracker Team",
-                
                 'planttrackerapp@gmx.de',
                 [request.data['email']],
                 fail_silently=False,
@@ -135,16 +133,14 @@ class CreateResetLink(generics.CreateAPIView):
                 send_mail(
                 'Your reset link for the Planttracker Project',
                 f"\
-                Dear Nature Lover🌻!
-
-                You have requested a password reset link.
-                Please reset your password by clicking on the following link:
-                
-                https://planttracker.onrender.com/#/reset?{uuid}.
-                
-                See you soon on The Planttracker Project!
-                ",
-                
+                Dear Nature Lover🌻!\
+                \
+                You have requested a password reset link.\
+                Please reset your password by clicking on the following link:\
+                \
+                https://planttracker.onrender.com/#/reset?{uuid}.\
+                \
+                See you soon on The Planttracker Project!",
                 'planttrackerapp@gmx.de',
                 [email],
                 fail_silently=False,
