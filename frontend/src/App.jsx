fdd1f2293588054b93ea34d1940a05cd708b29fb
gros-pataplouf <sceres@pm.me>
2023-07-09 13:26:56 +0200

    fix: further fixes to mobile frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/elements/Header";
-
+import { useEffect } from "react";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(
     Boolean(localStorage.getItem("planttrackerAccess"))
