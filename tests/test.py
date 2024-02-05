f15d1d98da012fd06882e76d241d5e3a796fe03d
gros-pataplouf <sceres@pm.me>
2024-02-05 22:12:57 +0100

    test: set up db fixture

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,26 @@
 import pytest
+from django.urls import reverse
+from pytest_drf import APIViewTest, Returns200, UsesGetMethod
+from pytest_lambda import lambda_fixture
 
-def test_example():
-    assert True
+from accounts.models import User
+from api.models import Plant
+
+@pytest.fixture
+def dummyuser(db):
+    user = User.objects.create_user('dummyuser1', 'test@pytest.com', 'pydummyuser')
+    return user
+
+def test_user(dummyuser):
+   assert dummyuser.username == 'dummyuser1'
+
+@pytest.mark.django_db(True)
+class TestPlant(
+    APIViewTest,
+    UsesGetMethod,
+    Returns200,
+):
+    url = lambda_fixture(lambda: reverse('api_plant_list'))
+    def test_it_returns_plantlist(self, json):
+        print("plantlist:", self, json)
+        assert True
