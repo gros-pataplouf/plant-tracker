4e851aa70078e9b72887183c78a4e08506524f51
gros-pataplouf <sceres@pm.me>
2024-02-20 16:44:34 +0100

    test: authenticated user can get their account details

--- conftest.py
+++ conftest.py
@@ -1,12 +1,27 @@
 import pytest
-
+from django.urls import reverse
 from pytest_factoryboy import register
 from  tests.factories import UserFactory
+from tests.factories import fake_password
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from rest_framework.test import APIClient
+register(UserFactory)
 
 @pytest.fixture
 def client():
     return APIClient()
 
-register(UserFactory)
+@pytest.fixture
+def unauthenticated_user(user_factory, db):
+    return user_factory.create()
+
+@pytest.fixture
+def authuser_clientheaders(user_factory, db):
+    client = APIClient()
+    user = user_factory.create()
+    endpoint = reverse('token_obtain_pair')
+    request = client.post(endpoint, {'username': user.username, 'password': fake_password}, format='json')
+    client.credentials(HTTP_AUTHORIZATION='JWT ' + request.data.get('access'))
+    return user, client
+
--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -4,12 +4,26 @@ from factories import fake_password
 from core.settings import SECRET_KEY
 
 
-def test_can_get_tokens(client, user_factory, db):
+
+def test_can_get_tokens(client, unauthenticated_user, db):
     endpoint = reverse('token_obtain_pair')
-    user = user_factory.create()
-    response = client.post(endpoint, {'username': user.username, 'password': fake_password}, format='json')
-    assert response.status_code == 200
-    access_token = jwt.decode(jwt=response.data.get("access"), key=SECRET_KEY, algorithms=["HS256"])
-    assert access_token.get('user_id')
-    refresh_token = jwt.decode(jwt=response.data.get("access"), key=SECRET_KEY, algorithms=["HS256"])
-    assert refresh_token.get('user_id')
+    request = client.post(endpoint, {'username': unauthenticated_user.username, 'password': fake_password}, format='json')
+    assert request.status_code == 200
+    decoded_access_token = jwt.decode(jwt=request.data.get("access"), key=SECRET_KEY, algorithms=["HS256"])
+    assert decoded_access_token.get('user_id')
+    decoded_refresh_token = jwt.decode(jwt=request.data.get("access"), key=SECRET_KEY, algorithms=["HS256"])
+    assert decoded_refresh_token.get('user_id')
+
+
+def test_unauthenticated_no_account_details(client, db):
+    endpoint = reverse('accounts_me')
+    request = client.get(endpoint)
+    assert request.status_code == 401
+
+
+def test_authenticated_gets_account_details(authuser_clientheaders, db):
+    [user, client] = authuser_clientheaders
+    endpoint = reverse('accounts_me')
+    request = client.get(endpoint)
+    assert request.status_code == 200
+
