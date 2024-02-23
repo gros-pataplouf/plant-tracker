c334a3870285348a6cc11527e41645d442dd1c8b
gros-pataplouf <sceres@pm.me>
2024-02-23 21:22:21 +0100

    test: author can delete his own location

--- api/views.py
+++ api/views.py
@@ -1,13 +1,9 @@
-import json, re, requests
-from datetime import datetime
-
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-
 from rest_framework.response import Response
 from rest_framework import generics, status
 from rest_framework.decorators import permission_classes, authentication_classes
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -23,24 +19,20 @@ class PlantList(generics.ListAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
     
-
 class PlantDetail(generics.RetrieveAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
-
 
 class LocationList(generics.ListCreateAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
     permission_classes = [ IsAuthenticatedOrReadOnly ]
     parser_classes = [ MultiPartParser, FormParser ]
-
     
     def get_queryset(self): # needed for map filtering feature
         query = self.request.GET
         return Location.objects.filter(**query.dict())
-    
-   
+
     def create(self, request, format=None):
         auth_data = JWT_authenticator.authenticate(request)
         if auth_data is not None:
@@ -49,12 +41,12 @@ class LocationList(generics.ListCreateAPIView):
             setattr(request.data, '_mutable', True)
             request.data['author'] = token.payload['user_id']
             # get coordinates out of query dict
-            #conditions for valid coordinations: key "location", length 2, both are floats
+            #conditions for valid coordinatess: key "location", length 2, both are floats
             request.data['display_name'] = get_osm_display_name(request.data)
             location_serializer = LocationSerializer(data=request.data)
             if location_serializer.is_valid():
                 location_serializer.save()
-                #step 2 : now get the id of the new location object and save the images
+                # get the id of the new location object and save the images
                 location_id = location_serializer.data['id']
                 if request.FILES:
                     for lst in dict(request.FILES).values():
@@ -66,19 +58,16 @@ class LocationList(generics.ListCreateAPIView):
                                 return Response("There seems to be a problem with your image file.", status=status.HTTP_400_BAD_REQUEST)
                 return Response(location_id, status=status.HTTP_201_CREATED)
             else: 
-                print(location_serializer.errors)
                 return Response("Data submitted are invalid or incomplete.", status=status.HTTP_400_BAD_REQUEST)
         return Response("Unauthorized", status=status.HTTP_401_UNAUTHORIZED)
 
-class LocationDetail(generics.RetrieveAPIView):
+class LocationDetail(generics.RetrieveDestroyAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
-
 
 class PlantImages(generics.ListAPIView):
     queryset = PlantImage.objects.all()
     serializer_class = PlantImageSerializer
-
 
 class LocationImages(generics.ListCreateAPIView):
     permission_classes = [ IsAuthenticatedOrReadOnly ]
@@ -86,4 +75,3 @@ class LocationImages(generics.ListCreateAPIView):
     def get_queryset(self):
         query = self.request.GET
         return LocationImage.objects.filter(**query.dict())
-
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
-import pytest, json
+import pytest, json, os
 from django.urls import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
 from api.models import Plant
 
 def test_can_get_plants_without_token(plants_db, client, db):
@@ -8,13 +9,11 @@ def test_can_get_plants_without_token(plants_db, client, db):
     assert request.status_code == 200
     assert len(request.data) == len(plants_db)
 
-
 def test_can_get_plant_detail_without_token(client, plants_db, db):
     existing_plant_id = Plant.objects.filter(scientific_name="Plantum plantum")[0].id
     endpoint = reverse("api_plant_detail", args=[existing_plant_id])
     request = client.get(endpoint)
     assert request.status_code == 200
-
 
 def test_can_get_location_list_without_token(client, db):
     endpoint = reverse("api_location_list")
@@ -31,28 +30,31 @@ def test_cannot_create_location_without_token(client):
     request = client.post(endpoint, {"data": "some gibberish"})
     assert request.status_code == 401
 
-
 def test_invalid_location_data_with_token_return_400(authuser_clientheaders, db):
     [user, client] = authuser_clientheaders
     endpoint = reverse("api_location_list")
     request = client.post(endpoint, {"data": "some gibberish"})
     assert request.status_code == 400
 
-
-def test_valid_location_data_with_token_returns_201(authuser_clientheaders, plants_db, db):
+def test_can_upload_image_file_with_location(authuser_clientheaders, plants_db, db):
     [user, client] = authuser_clientheaders
     existing_plant = Plant.objects.filter(scientific_name="Plantum plantum")
     endpoint = reverse("api_location_list")
-    request = client.post(endpoint, {
-        "plant": existing_plant[0].id,
-        "area": 1,
-        "location": 
-        json.dumps({ #must be converted to string bc multipart requests cannot be nested
-            "type": "Point",
-            "coordinates": [50.123, -10.123]}),
-        },
-        format='multipart')
-    assert request.status_code == 201
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    with open(f"{test_dir}/data/testphoto.jpg", "rb") as testfile:
+        img = SimpleUploadedFile(f"{test_dir}/testphoto.jpg", testfile.read(), content_type='multipart/form-data')
+        request = client.post(endpoint, {
+            "plant": existing_plant[0].id,
+            "area": 1,
+            "location": 
+            json.dumps({ #must be converted to string bc multipart requests cannot be nested
+                "type": "Point",
+                "coordinates": [50.123, -10.123]}),
+            "images" : [img]
+            },
+            format='multipart'
+            )
+        assert request.status_code == 201
 
 def test_can_get_plant_images_without_auth(client, db):
     endpoint = reverse("api_plant_images")
@@ -68,3 +70,23 @@ def test_can_get_location_detail_without_auth(client, db):
     endpoint = reverse("api_location_detail", args=[3])
     request = client.get(endpoint)
     assert request.status_code == 200 or 404
+
+def test_author_can_delete_location(authuser_clientheaders, plants_db, db):
+    endpoint_list = reverse("api_location_list")
+    [user, client] = authuser_clientheaders
+    existing_plant = Plant.objects.filter(scientific_name="Plantum plantum")
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    request = client.post(endpoint_list, {
+            "plant": existing_plant[0].id,
+            "area": 1,
+            "location": 
+            json.dumps({ #must be converted to string bc multipart requests cannot be nested
+                "type": "Point",
+                "coordinates": [50.123, -10.123]})
+                },
+            format='multipart'
+            )
+    location_id = request.data
+    endpoint_detail = reverse("api_location_detail", args=[location_id])
+    deletion_request = client.delete(endpoint_detail)
+    assert deletion_request.status_code == 204
